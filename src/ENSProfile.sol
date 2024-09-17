551a89b6ab0f0a05a736865d5d8a48cb33232220
nxt3d <pm@premmakeig.com>
2024-09-17 00:48:01 -0400

    update pragma version

--- src/ENSProfile.sol
+++ src/ENSProfile.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
 
 import "openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "openzeppelin/contracts/access/AccessControl.sol";
